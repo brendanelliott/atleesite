7a1c0c4aca321f1b30e9076ce399eea4e8adf65a
Brendan <bae48@cornell.edugit config --global user.name Brendanelliott>
2019-12-04 01:31:08 -0800

    Tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Atlee Design',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon-32x32.png`,
+      },
+    },
+  ],
 }
